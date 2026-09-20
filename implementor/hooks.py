6f12b3874ffb07090adbf272501f34212982a194
Hyrin-mansoor <hyrin@erpgulf.com>
2026-09-20 06:46:07 +0300

    Updated Scheduled event timing for email summary

--- implementor/hooks.py
+++ implementor/hooks.py
@@ -189,32 +189,25 @@ doc_events = {
 
 #     },
 # }
+# 
 # scheduler_events = {
 #     "cron": {
 #         "*/15 * * * *": [
 #             "implementor.implementor.auth.send_daily_task_summary",
-#             "implementor.implementor.auth.send_weekly_task_summary"
+#             "implementor.implementor.auth.send_weekly_task_summary",
 #         ],
 #     }
 # }
 scheduler_events = {
     "cron": {
-        "*/15 * * * *": [
-            "implementor.implementor.auth.send_daily_task_summary",
-            "implementor.implementor.auth.send_weekly_task_summary",
+        "0 22 * * *": [
+            "implementor.implementor.auth.send_daily_task_summary"
+        ],
+        "0 22 * * 0": [
+            "implementor.implementor.auth.send_weekly_task_summary"
         ],
     }
 }
-# scheduler_events = {
-#     "cron": {
-#         "0 22 * * *": [
-#             "implementor.implementor.auth.send_daily_task_summary"
-#         ],
-#         "0 22 * * 0": [
-#             "implementor.implementor.auth.send_weekly_task_summary"
-#         ],
-#     }
-# }
 
 # Testing
 # -------
